167732df999adc16e3251e02785178c84febb55a
mshtrk602-pixel <mshtrk602@gmail.com>
2026-08-17 20:42:04 +0300

    Update process.py

--- process.py
+++ process.py
@@ -3,11 +3,11 @@ from googleapiclient.discovery import build
 import json
 import os
 
-# قراءة الـ Credentials من الـ Environment Variable (GitHub Secret)
+# قراءة بيانات الـ Secret من بيئة العمل
 creds_json = os.environ.get("YOUTUBE_CREDENTIALS")
 creds_dict = json.loads(creds_json)
 
-# استخدام حساب الخدمة (Service Account)
+# اعتماد حساب الخدمة بالشكل الصحيح
 credentials = service_account.Credentials.from_service_account_info(
     creds_dict, scopes=["https://www.googleapis.com/auth/youtube.upload"]
 )
